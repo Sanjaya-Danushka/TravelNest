dfc51cd2be853a00fd1bdf2e70de71d39271fd5a
Sanjaya Danushka <dsanjaya712@gmail.com>
2026-03-08 11:30:42 +0530

    home page fully initialized

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -14,10 +14,253 @@ class _HomeState extends State<Home> {
       // ignore: avoid_unnecessary_containers
       body: Container(
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Image.asset(
-              "assets/images/home1.jpg",
-              width: MediaQuery.of(context).size.width,
+            Stack(
+              children: [
+                Image.asset(
+                  "assets/images/home3.jpg",
+                  width: MediaQuery.of(context).size.width,
+                  height: MediaQuery.of(context).size.height / 2.5,
+                  fit: BoxFit.cover,
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 40, right: 20, left: 20),
+                  child: Row(
+                    // mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Material(
+                        elevation: 3.0,
+                        borderRadius: BorderRadius.circular(10),
+                        child: Container(
+                          padding: EdgeInsets.all(8),
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          child: Image.asset(
+                            "assets/images/logo.png",
+                            width: 35,
+                            height: 35,
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                      ),
+                      Spacer(),
+                      Material(
+                        elevation: 3.0,
+                        borderRadius: BorderRadius.circular(10),
+                        child: Container(
+                          padding: EdgeInsets.all(8),
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          child: Icon(Icons.add, color: Colors.blue, size: 30),
+                        ),
+                      ),
+                      SizedBox(width: 10),
+                      Material(
+                        elevation: 3.0,
+                        borderRadius: BorderRadius.circular(60),
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(60),
+                          child: Image.asset(
+                            "assets/images/user.jpg",
+                            width: 50,
+                            height: 50,
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 160, left: 20),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "TravelNest",
+                        style: TextStyle(
+                          color: const Color.fromARGB(255, 77, 77, 77),
+                          fontFamily: "Lato",
+                          fontSize: 50,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                      Text(
+                        "Explore the world your way",
+                        style: TextStyle(
+                          color: Colors.white60,
+                          fontFamily: "poppins",
+                          fontSize: 20,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.only(
+                    left: 20,
+                    right: 20,
+                    top: MediaQuery.of(context).size.height / 2.7,
+                  ),
+
+                  child: Material(
+                    borderRadius: BorderRadius.circular(10),
+                    elevation: 5.0,
+                    child: Container(
+                      padding: EdgeInsets.symmetric(horizontal: 20),
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(10),
+                        border: Border.all(width: 1.5),
+                      ),
+                      child: TextField(
+                        decoration: InputDecoration(
+                          border: InputBorder.none,
+                          contentPadding: EdgeInsets.symmetric(vertical: 15),
+                          hintText: "Search for destinations",
+                          suffixIcon: Icon(Icons.search),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 40),
+            Container(
+              margin: EdgeInsets.only(left: 40, right: 30),
+              child: Material(
+                elevation: 3.0,
+                borderRadius: BorderRadius.circular(20),
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(20),
+                    // border: Border.all(width: 1.5),
+                  ),
+
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(top: 20, left: 10),
+                        child: Row(
+                          children: [
+                            ClipRRect(
+                              borderRadius: BorderRadius.circular(30),
+                              child: Image.asset(
+                                "assets/images/user.jpg",
+                                width: 50,
+                                height: 50,
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                            SizedBox(width: 10),
+                            Text(
+                              "Alexa Smith",
+                              style: TextStyle(
+                                color: const Color.fromARGB(153, 0, 0, 0),
+                                fontFamily: "poppins",
+                                fontSize: 20,
+                                fontWeight: FontWeight.w500,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      SizedBox(height: 20),
+                      Image.asset(
+                        "assets/images/homebg.jpg",
+
+                        // width: 20,
+                        // height: 20,
+                      ),
+                      SizedBox(height: 5),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 10),
+                        child: Row(
+                          children: [
+                            Icon(
+                              Icons.location_on,
+                              color: Colors.blue,
+                              size: 20,
+                            ),
+                            SizedBox(width: 5),
+                            Text(
+                              "Ella Rock, Sri Lanka",
+                              style: TextStyle(
+                                color: const Color.fromARGB(255, 9, 9, 9),
+                                fontFamily: "poppins",
+                                fontSize: 15,
+                                fontWeight: FontWeight.w500,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      SizedBox(height: 5),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 10),
+                        child: Text(
+                          "Discover places most travelers miss",
+                          style: TextStyle(
+                            color: const Color.fromARGB(153, 0, 0, 0),
+                            fontFamily: "poppins",
+                            fontSize: 15,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 10),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 20),
+                        child: Row(
+                          children: [
+                            Icon(
+                              Icons.favorite_outline,
+                              color: Colors.black54,
+                              size: 25,
+                            ),
+                            SizedBox(width: 10),
+                            Text(
+                              "Like",
+                              style: TextStyle(
+                                color: Colors.black,
+                                fontFamily: "poppins",
+                                fontSize: 18,
+                                fontWeight: FontWeight.w500,
+                              ),
+                            ),
+                            SizedBox(width: 30),
+                            Icon(
+                              Icons.comment_outlined,
+                              color: Colors.black54,
+                              size: 25,
+                            ),
+                            SizedBox(width: 10),
+                            Text(
+                              "comment",
+                              style: TextStyle(
+                                color: Colors.black,
+                                fontFamily: "poppins",
+                                fontSize: 18,
+                                fontWeight: FontWeight.w500,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      SizedBox(height: 20),
+                    ],
+                  ),
+                ),
+              ),
             ),
           ],
         ),
